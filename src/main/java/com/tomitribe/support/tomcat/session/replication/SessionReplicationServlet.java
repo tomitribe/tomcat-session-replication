01b1b64d0d5a011a6cefffc1405bfc54cb65bf5e
Roberto Cortez <radcortez@yahoo.com>
2017-11-14 15:41:43 +0000

    Initial version.

---
+++ src/main/java/com/tomitribe/support/tomcat/session/replication/SessionReplicationServlet.java
@@ -0,0 +1,41 @@
+package com.tomitribe.support.tomcat.session.replication;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Serializable;
+
+@WebServlet(name = "replication", urlPatterns = "/")
+public class SessionReplicationServlet extends HttpServlet {
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
+            throws ServletException, IOException {
+        final HttpSession session = req.getSession();
+        final Object data = session.getAttribute("data");
+        if (data != null) {
+            ((Data) data).incrementCounter();
+        } else {
+            session.setAttribute("data", new Data());
+        } 
+        
+        resp.getWriter().write(session.getId());
+        resp.getWriter().write("<br>data:" + session.getAttribute("data"));
+    }
+
+    public static class Data implements Serializable {
+        private int counter = 0;
+
+        public void incrementCounter() {
+            counter ++;
+        }
+
+        @Override
+        public String toString() {
+            return "Data{counter=" + counter + "}";
+        }
+    }
+}
