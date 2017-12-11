54f2fed4cb7ee6059fde72624abc132869a73668
Jonathan Gallimore <jon@jrg.me.uk>
2017-12-11 22:18:27 +0000

    Fix package

--- environments/src/main/java/com/tomitribe/support/tomcat/session/replication/SessionReplicationServlet.java
+++ environments/src/main/java/com/tomitribe/support/tomcat/session/replication/SessionReplicationServlet.java
@@ -1,4 +1,4 @@
-package main.java.com.tomitribe.support.tomcat.session.replication;
+package com.tomitribe.support.tomcat.session.replication;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
